7eb6b8abe2ecccf80cdfd73c23dcbe406c36e015
Benjamin <benjamin.alazet@gmail.com>
2023-05-30 16:29:08 +0200

    modify for number of agruments > 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
         print("{} argument:".format(i))
         print("1: {}".format(sys.argv[1]))
     else:
-        i = 1
         print("{} arguments:".format(i))
-        for arg in (sys.argv):
-            if arg != 0:
-                print("{}: {}".format(i, arg))
-                i += 1
+        i = 1
+        for arg in sys.argv[1:]:
+            print("{}: {}".format(i, arg))
+            i += 1
