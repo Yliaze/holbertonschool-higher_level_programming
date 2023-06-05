9a32261a240cd700e5de4df9040872c75f0a92a0
Benjamin <benjamin.alazet@gmail.com>
2023-06-05 14:04:08 +0200

    Function that removes "c" and "C" from a string

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+
+    for i in my_string:
+        if ord(i) != 99 and ord(i) != 67:
+            new_string += i
+
+    return new_string
