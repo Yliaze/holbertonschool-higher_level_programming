435e8a4509902624f27f2cd5024de969be4df057
Benjamin <benjamin.alazet@gmail.com>
2023-06-05 14:46:31 +0200

    Modify matrix function for spaces between integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix:
-        for line in range(len(matrix)):
-            for element in matrix[line]:
-                print("{:d}".format(element), end="")
+        for line in matrix:
+            for element in range(len(line)):
+                if element == len(line) - 1:
+                    print("{:d}".format(line[element]), end="")
+                else:
+                    print("{:d} ".format(line[element]), end="")
             print()
