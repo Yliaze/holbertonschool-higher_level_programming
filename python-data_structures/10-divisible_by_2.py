115f40968c5d6bb62d609b0a0c59ae09acdd8d3d
Benjamin <benjamin.alazet@gmail.com>
2023-06-05 17:55:59 +0200

    Forgot a slash after #!

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,6 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
 
     new_list = []
-
-    for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-
+    new_list = [True if i % 2 == 0 else False for i in my_list]
     return new_list
