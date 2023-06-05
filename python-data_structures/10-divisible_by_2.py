ca7a093b6fe5a1ba145823effe70e06b44536b3e
Benjamin <benjamin.alazet@gmail.com>
2023-06-05 17:41:11 +0200

    Add some checks and modify comprehension

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!usr/bin/python3
 def divisible_by_2(my_list=[]):
 
-    new_list = []
-    new_list = [True if i % 2 == 0 else False for i in my_list]
+    if my_list is None:
+        my_list = []
+    new_list = [i % 2 == 0 for i in my_list]
     return new_list
