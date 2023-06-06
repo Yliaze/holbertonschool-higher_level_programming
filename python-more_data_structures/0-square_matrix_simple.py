f8cd4023b712e58a8b533c27ce545139c95224c2
Benjamin <benjamin.alazet@gmail.com>
2023-06-06 14:13:55 +0200

    Fct compute the square value of all ints of matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix == []:
+        return matrix
+
+    new_matrix = [[element**2 for element in row] for row in matrix]
+    return new_matrix
