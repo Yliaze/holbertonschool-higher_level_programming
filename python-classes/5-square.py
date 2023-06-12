e45861b5d0e5b303769c8d80e873b2a42ad7f048
Benjamin <benjamin.alazet@gmail.com>
2023-06-12 14:18:34 +0200

    Square Class & add methodthat prints square with #

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Defines a square by size (private instance attribute)"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    """Property def size(self): to retrieve it"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Property setter def size(self, value): to set it"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Returns the current square area"""
+    def area(self):
+        return self.__size * self.__size
+
+    """Prints in stdout the square with the character #"""
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            print("#" * self.__size)
