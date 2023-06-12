e3357cb5d4821331611b06eece259e9b3c8f3778
Benjamin <benjamin.alazet@gmail.com>
2023-06-12 15:31:44 +0200

    Add some check for the position.setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,6 +33,8 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(num, int) for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
