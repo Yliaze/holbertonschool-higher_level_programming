fdd3dd59aa87f57bf4548cfc83279055f887106e
Benjamin <benjamin.alazet@gmail.com>
2023-06-12 15:34:28 +0200

    Add a return to self.__size == 0

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -46,6 +46,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
+            return
 
         for i in range(self.__position[1]):
             print("")
