8a4eb3e4ec7a3129ca981972314915edcd1146f7
Benjamin <benjamin.alazet@gmail.com>
2023-06-13 11:47:56 +0200

    Try new conditions

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
     """Property setter def position(self, value): to set it"""
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        if type(value) is not tuple or len(value) is not 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not all(isinstance(num, int) and num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -46,11 +46,10 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print("")
-            return
+        else:
+            for i in range(self.__position[1]):
+                print("")
 
-        for i in range(self.__position[1]):
-            print("")
-
-        for row in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+            for row in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
