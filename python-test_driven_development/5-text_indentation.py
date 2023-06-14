c6a8bb1eca8af940b70b4bbb92dcaaf4534c1571
Benjamin <benjamin.alazet@gmail.com>
2023-06-14 11:57:45 +0200

    Fct prints text with 2 \n after separators & tests

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module that prints a text with 2 new lines after each
+of these characters: ., ? and :
+    Args:
+        text (string): text to indent
+"""
+
+
+def text_indentation(text):
+    """Function that prints a text with 2 new lines after each
+    of these characters: ., ? and :
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    separators = [".", "?", ":"]
+    result = ""
+    new_line = True
+
+    for i in text:
+        if new_line and i == " ":
+            continue
+        result += i
+        if i in separators:
+            result += "\n\n"
+            new_line = True
+        else:
+            new_line = False
+
+    print(result.strip(), end="")
