58e6aba4f4562baf6fb0f3fc90e0372fa4bece99
Benjamin <benjamin.alazet@gmail.com>
2023-06-20 10:07:59 +0200

    Add fct that raise an exception "not implemented"

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry :
+-description-
+
+Args:
+    -NA (type): NA-
+"""
+
+
+class BaseGeometry():
+    """Class BaseGeometry"""
+    def area(self):
+        """Raise an Exception with a message"""
+        raise Exception("area() is not implemented")
