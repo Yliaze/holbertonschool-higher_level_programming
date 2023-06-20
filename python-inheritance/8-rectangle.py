c584c2eecc989183f576d18e7aed78cd83f393a3
Benjamin <benjamin.alazet@gmail.com>
2023-06-20 12:05:03 +0200

    Create class Rectangle that inherits BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry: Superclass
+
+class Rectangel: Subclass
+
+Args:
+    width (int): width of the rectangle
+    height (int): height of the rectangle
+"""
+
+
+class BaseGeometry():
+    """Class BaseGeometry"""
+    def area(self):
+        """Raise an Exception with a message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle, subclass of BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation using BaseGeometry(superclass) function"""
+        super().integer_validator("width", width)
+        super().integer_validator("heigth", height)
+        self.__width = width
+        self.__height = height
