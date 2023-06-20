fc8ec6294fac04b857e012eecba9585d390af021
Benjamin <benjamin.alazet@gmail.com>
2023-06-20 12:12:35 +0200

    Try with import solution

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,31 +1,15 @@
 #!/usr/bin/python3
-"""
-class BaseGeometry: Superclass
-
-class Rectangel: Subclass
-
-Args:
-    width (int): width of the rectangle
-    height (int): height of the rectangle
-"""
-
-
-class BaseGeometry():
-    """Class BaseGeometry"""
-    def area(self):
-        """Raise an Exception with a message"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates value"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""class Rectangle: subclass that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle, subclass of BaseGeometry"""
+    """Class Rectangle, subclass of BaseGeometry
+
+    Args:
+        width (int): width of the rectangle
+        height (int): height of the rectangle
+    """
     def __init__(self, width, height):
         """Instantiation using BaseGeometry(superclass) function"""
         super().integer_validator("width", width)
