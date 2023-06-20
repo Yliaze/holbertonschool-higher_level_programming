02b3dac6d747a021bbdbaaf19c269fe922a129a4
Benjamin <benjamin.alazet@gmail.com>
2023-06-20 12:44:36 +0200

    Modify __str__ description

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class Square: subclass that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """Class Square, subclass of BaseGeometry
+
+    Args:
+        size (int): size of the square
+    """
+    def __init__(self, size):
+        """Instantiation using BaseGeometry(superclass) function"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Return description of the rectangle/square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
