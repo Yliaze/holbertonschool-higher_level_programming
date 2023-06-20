67e68aa2a12b8f2ca0ea232b715fa67c8a165c48
Benjamin <benjamin.alazet@gmail.com>
2023-06-20 10:46:09 +0200

    Add fct with Type and ValueErrors. Add tests file

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry :
+-description-
+
+Args:
+    -NA (type): NA-
+"""
+
+
+class BaseGeometry():
+    """Class BaseGeometry"""
+    def area(self):
+        """Raise an Exception with a message"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
