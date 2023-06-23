4836bf9d51f6c60910168a63c673021f02c045a9
Benjamin <benjamin.alazet@gmail.com>
2023-06-23 09:50:51 +0200

    Modify docstring

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -4,5 +4,5 @@
 
 def class_to_json(obj):
     """Function that returns the dictionary description with
-    simple data structure for JSON serialization of an object:"""
+    simple data structure for JSON serialization of an object"""
     return (obj.__dict__)
