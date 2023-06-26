418717573c7a4a6c92dfd7427f84685439c36aef
Benjamin <benjamin.alazet@gmail.com>
2023-06-26 10:53:09 +0200

    Modify private attrs to public for getter/setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,39 +17,47 @@ class Rectangle(Base):
             y (int): The y coordinate of the new Rectangle
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def widht(self):
+        """Getter for width"""
         return self.__width
 
     @widht.setter
     def widht(self, value):
+        """Setter for width"""
         self.__width = value
 
     @property
     def height(self):
+        """Getter for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Setter for heigth"""
         self.__height = value
 
     @property
     def x(self):
+        """Getter for x"""
         return self.__x
 
     @widht.setter
     def x(self, value):
+        """Setter for x"""
         self.__x = value
 
     @property
     def y(self):
+        """Getter for y"""
         return self.__y
 
     @height.setter
     def y(self, value):
+        """Setter for y"""
         self.__y = value
