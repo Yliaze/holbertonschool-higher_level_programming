7a9eaa4dda8f1f1e7a059cc6687b87a95c0856f8
Benjamin <benjamin.alazet@gmail.com>
2023-06-26 10:34:32 +0200

    Add class Rectangle that inherits from Base

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class Rectangle : Inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Base
+
+        Args:
+            id (int): The identity of the new Rectangle
+            width (int): The width of the new Rectangle
+            height (int): The height of the new Rectangle
+            x (int): The x coordinate of the new Rectangle
+            y (int): The y coordinate of the new Rectangle
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
