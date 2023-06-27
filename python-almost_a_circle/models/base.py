20aaecbafffc3b3a150889573f2464d7201ef8ee
Benjamin <benjamin.alazet@gmail.com>
2023-06-27 10:14:45 +0200

    Add decorator  @staticmethod

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,12 +23,13 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries
 
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
