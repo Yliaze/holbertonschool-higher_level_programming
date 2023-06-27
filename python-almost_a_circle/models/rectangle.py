e39ea22f0e3ecf966e2c1af3d96d098fb7aa4265
Benjamin <benjamin.alazet@gmail.com>
2023-06-27 10:08:40 +0200

    Change for base static method json

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -134,8 +134,8 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Rectangle"""
-        return {"x": self.x, "y": self.y, "id": self.id,
-                "height": self.height, "width": self.width}
+        return {"x": self.x, "width": self.width, "id": self.id,
+                "height": self.height, "y": self.y}
 
     def __str__(self):
         """Return description of the rectangle"""
