a80a3f07bfa51e8bd38375c70c3688f960bd884b
Benjamin <benjamin.alazet@gmail.com>
2023-06-29 10:43:53 +0200

    Peers learning for all tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -2,75 +2,53 @@
 """Unittest for Base Class in base.py"""
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestBase(unittest.TestCase):
 
-    def test_id_empty(self):
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-
-    def test_id(self):
-        base = Base(12)
-        self.assertEqual(base.id, 12)
+    def setUp(self):
+        self.base = Base(12)
 
     def test_nb_objects(self):
+        # test nb_object before first call
         Base.__nb_objects = 0
 
+    def test_ID(self):
+        # if id, id = value
+        base = Base(39)
+        self.assertEqual(base.id, 39)
 
-class TestStaticMethods(unittest.TestCase):
+    def test_IDNone(self):
+        # if no id, id = nb_obj
+        base1 = Base()
+        self.assertEqual(base1.id, 1)
 
-    def test_to_json_string_rectangle_type(self):
-        r = Rectangle(10, 7, 2, 8, 6)
-        self.assertEqual(str, type(Base.to_json_string([r.to_dictionary()])))
 
-    def test_to_json_string_square_type(self):
-        s = Square(10, 7, 2, 8)
-        self.assertEqual(str, type(Base.to_json_string([s.to_dictionary()])))
+class TestBaseStaticMethod(unittest.TestCase):
 
-    def test_to_json_string_empty_list(self):
-        json_string = []
-        self.assertEqual("[]", Base.to_json_string(json_string))
+    def setUp(self):
+        self.base = Base(12)
 
-    def test_to_json_string_none(self):
+    def test_outputTypeToJsonString(self):
+        # test output function is a string
+        list_dict = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+        self.assertEqual(type(Base.to_json_string(list_dict)), str)
+
+    def test_outputEmptyToJsonString(self):
+        # test output function if None as input is a string empty list
+        list_dict = None
+        self.assertEqual(Base.to_json_string(list_dict), '[]')
+
+    def test_outputTypeFromJsonString(self):
+        # test output function is a list
+        json_string = '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]'
+        self.assertEqual(type(Base.from_json_string(json_string)), list)
+
+    def test_outputEmptyTypeFromJsonString(self):
+        # test output function if None as input is a empty list
         json_string = None
-        self.assertEqual("[]", Base.to_json_string(json_string))
+        self.assertEqual(Base.from_json_string(json_string), [])
 
 
-class TestClassMethods(unittest.TestCase):
-
-    def test_from_json_string_rectangle(self):
-        list_input = [{"id": 89, "width": 10, "height": 4, "x": 7}]
-        json_list_input = Rectangle.to_json_string(list_input)
-        list_output = Rectangle.from_json_string(json_list_input)
-        self.assertEqual(list_input, list_output)
-
-    def test_from_json_string_square(self):
-        list_input = [{"id": 89, "size": 10, "height": 4}]
-        json_list_input = Square.to_json_string(list_input)
-        list_output = Square.from_json_string(json_list_input)
-        self.assertEqual(list_input, list_output)
-
-    def test_from_json_string_empty_list(self):
-        self.assertEqual([], Base.from_json_string("[]"))
-
-    def test_from_json_string_None(self):
-        self.assertEqual([], Base.from_json_string(None))
-
-    def test_create_rectangle(self):
-        r1 = Rectangle(3, 5, 1, 2, 7)
-        r1_dictionary = r1.to_dictionary()
-        r2 = Rectangle.create(**r1_dictionary)
-        self.assertEqual("[Rectangle] (7) 1/2 - 3/5", str(r1))
-
-    def test_create_square(self):
-        s1 = Square(3, 5, 1, 7)
-        s1_dictionary = s1.to_dictionary()
-        s2 = Square.create(**s1_dictionary)
-        self.assertEqual("[Square] (7) 5/1 - 3", str(s1))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
